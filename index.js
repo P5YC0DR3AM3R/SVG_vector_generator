7766d79d71727941c2d6ed44fcfaecc7279c933d
Micah Read <164657374+P5YC0DR3AM3R@users.noreply.github.com>
2024-04-29 21:58:25 -0400

    shape and text generation is working, 1 failing test

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const colors = require("colors");
-const shapes = require("./lib/shapes")
-
-inquirer
-    .prompt(questions)
-        .then((answers) => {
-
-        })
-
-const questions = [
-    {
-        type: "input",
-        name: "text",
-        message: "Please enter 3 letters for logo text: "
-    }
-    {
-        type: "input",
-        name: "textColor",
-        message: "Please enter a color as a keyword, or hexadecimal number, for your text: "
-    }
-    {
-        type: "input",
-        name: "Shape",
-        message: "Please enter a color as a keyword, or hexadecimal number, for your text: "
-    }
-]
+const shapes = require("./lib/shapes");
+const CLI = require("./lib/cli");
+const jest = require("jest");
